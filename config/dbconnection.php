d195ad37e36292a4b3b29bd2951e237def9ccf3f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-20 12:25:25 -0700

    Update dbconnection.php

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -1,1 +1,13 @@
-connection database
+<?php
+$host='localhost';
+$database = 'library';
+$username = 'root';
+$password ='';
+
+try{
+  $pdo = new PDO('mysql:host=$host,dbname=$database,$user, $pass);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION;
+  } cath(PDOException $e) {
+    echo 'connection failed : '$e->getMessage();
+}
+?>
