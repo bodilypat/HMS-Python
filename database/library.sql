998ae80630986389c15b7ec03bdf8f67dde1eccf
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-29 08:47:04 -0700

    Add files via upload

--- database/library.sql
+++ database/library.sql
@@ -1,6 +1,11 @@
-CREATE DATABASE library;
-
-USE library;
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin','user','member') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE authors(
     id INT AUTO_INCREMENT KEY,
@@ -10,17 +15,22 @@ CREATE TABLE authors(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE categories(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+);
+
 CREATE TABLE books(
-    id INT AUTO_INCREMENT PRIMARTY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
-    pulishered_date DATE,
-    isbn VARCHAR(20) UNIQUE, 
-    available INT DEFAULT 1
+    isbn VARCHAR(20) UNIQUE,
+    published_date DATE,
+    available_copie INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE publishers(
-    id INT AUTO_INCREMENT KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     address VARCHAR(255),
     contact_number VARCHAR(20),
@@ -29,20 +39,32 @@ CREATE TABLE publishers(
 );
 
 CREATE TABLE borrows(
-    id INT AUTO_INCREMENT KEY,
-    user_id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     book_id INT,
-    borrow_date DATE,
-    return_date DATE,
+    user_name VARCHAR(255) NOT NULL,
+    borrow_date DATE NOT NULL,
+    return_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCE users(id);
     FOREIGN KEY (book_id) REFERENCE books(id);
 );
 
-CREATE TABLE users(
-    id INT AUTO_INCREMENT KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone VARCHAR(15);
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE loans(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    loan_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    due_date DATETIME,
+    returned BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
 );
+
+CREATE TABLE members(
+    id INT AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(15),
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
