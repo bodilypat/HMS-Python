b256ea5d6d69c28bba622d5518d5931333883a9d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-30 13:40:32 -0700

    Add files via upload

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,13 +1,19 @@
-from .base_model import Base
-from .user import User
-from .guest import Guest
-from .booking import Booking
-from .room import Room
-from .reservation import Reservation
-from .payment import Payment
-from .staff import Staff
-from .room_service import RoomService 
-from .feedback import Feedback
+# backend/app/models/__init__.py
+
+from app.models.core.base_model import Base
+from app.models.core.user import User
+from app.models.core.guest import Guest
+from app.models.core.staff import Staff
+
+from app.models.booking.booking import Booking
+from app.models.booking.reservation import Reservation
+
+from app.models.room.room import Room
+
+from app.models.finance.payment import Payment
+from app.models.finance.room_service import RoomService 
+
+from app.models.feedback.feedback import Feedback
 
 # Optional: List of all models fro bulk metadata operations
 __all__ = [
