99af4eb088bd43da2face17031afb3802b1b603d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-30 12:50:58 -0700

    Delete backend/routes directory

--- backend/routes/api_route.py
+++
@@ -1,7 +0,0 @@
-# backend/routers/api_router.py
-
-from fastapi import APIRouter
-from app.controllers import guest_controller
-
-router = APIRouter()
-router.include_router(guest_controller.router)
