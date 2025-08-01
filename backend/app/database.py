b1e31f5ee783365fa2d287f532eb2ed9a893a225
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-01 10:39:40 -0700

    Add files via upload

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from app.config.settings import get_settings
 
+# Create the SQLAlchemy engine
 engine = create_engine(get_settings().DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(bind=engine)
+
+# Create a configurated "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush= bind=engine)
 
 def int_db():
+    """
+        Initializes the database by creating all tables defined in the models.
+    """
 	from app.models.base_model import Base 
 	Base.metadata.create_all(bind=engine)
 	
