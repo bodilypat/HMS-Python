f2326ebdde3cc3fff89ba395546b3d6d480438f0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-04 08:28:53 -0700

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,17 +4,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
 
 from app.routes import api
-from app.database import init_db
+from app.config.database import Base, engine
+from app.config.setting import settings 
 
 def create_app() -> FastAPI:
-	# Initialize FastAPI app
-	app = FastAPI(
-		title ="Hotel Management System API",
-		version="1.0.0",
-        description="Backend API for managing hotel operations",
-    }
+	"""
+        Initialize FastAPI application.
+    """
     
-    # Middleware setup 
+# Middleware setup 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -23,14 +21,18 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
     
-    # Register API routes
+# Include register API routes
     app.include_router(api.router)
     
     return app
 
 app = create_app()
 
-# Initialize the database (create tables)
-init_db()
+# Run database table creation(avoid in production-use alembic migrations )
+@app.on_event("startup")
+def on_startup():
+    print("Creating database tables (if not exists) ...")
+    Base.metadata.create_all(bind=engine)
+
 
     
