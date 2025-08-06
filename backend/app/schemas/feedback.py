e8820ee78a12158b8342fb1eddc05e13649d8055
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-05 23:58:46 -0700

    Add files via upload

---
+++ backend/app/schemas/feedback.py
@@ -0,0 +1,35 @@
+# backend/app/schemas/feedback.py
+
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel,  Field, conint 
+
+class FeedbackBase(BaseModel):
+    guest_id: int 
+    reservation_id: int 
+    rating: conint(get=1, le=50 = Field(..., description="Rating must be between 1 and 5")
+    comments: Optional[str] = None 
+    feedback_date: Optional[datetime] = None 
+    
+class FeedbackCreate(FeedbackBase):
+    pass 
+    
+class FeedbackUpdate(BaseModel):
+    guest_id: Optional[int] = None 
+    reservation_id: Optional[int] = None 
+    rating: Optional[conint(get=1, le=5)] = None 
+    comment: Optional[str] = None
+    feedback_date: Optional[datetime] = None 
+    
+class FeedbackOut(BaseModel):
+    feedback_id: int 
+    guest_id: int 
+    reservation_id: int 
+    rating: int 
+    comment: Optional[str]
+    feedback_date: datetime
+    
+class Config:
+    orm_mode = True 
+    
+    
