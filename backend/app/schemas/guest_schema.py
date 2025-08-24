b7114efea9edb1b851f105feb49b9410ce7a4535
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-24 14:14:42 -0700

    Add files via upload

---
+++ backend/app/schemas/guest_schema.py
@@ -0,0 +1,54 @@
+# backend/app/schemas/guest.py
+
+from datetime import date, datetime 
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel, constr, EmailStr 
+
+class IDType(str, Enum):
+    passport = "Passport"
+    national_id = "National ID"
+    driver_license = "Drive Lincense"
+    
+class GuestBase(BaseModel):
+    first_name: constr(min_length=1, max_length=100)
+    last_name: constr(min_length=1, max_length=100)
+    email: EmailStr 
+    phone_number: Optional[constr(max_length=20)] = None
+    address: Optional[str] = None 
+    id_type: IDType
+    id_number: constr(min_length=3, max_length=50)
+    dob: date 
+    nationality: Optional[constr(max_length=50)]= "Unknown"
+    
+class GuestCreate(GuestBase):
+    pass
+    
+class GuestUpdate(BaseModel):
+    first_name: Optional[constr(min_length=1, max_length=100)] = None
+    last_name: Optional[constr(min_length=1, max_length=100)] = None
+    email: Optiona[EmailStr] = None
+    phone_number: optional[constr(max_length=20)] = None
+    address: Optional[str] = None
+    id_type: Optional[IDType] = None
+    id_number: Optional[constr(min_length=3, max_length=50) = None
+    dob: Optional[date] = None
+    nationality: Optional[constr(max_length=50)] = None 
+    
+class GuestOut(BaseModel):
+    guest_id: int
+    first_name: str
+    last_name: str 
+    email: EmailStr 
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
+    id_type: IDType 
+    id_number: str
+    dob: date 
+    nationality: str 
+    created_at: datetime
+    updated_at: datetime 
+    
+class Config:
+    orm_mode = True 
+    
