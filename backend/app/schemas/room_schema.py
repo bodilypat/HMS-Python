975c63a54c969535aed87b8780639619421ddf43
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-25 13:15:32 -0700

    Add files via upload

--- backend/app/schemas/room_schema.py
+++ backend/app/schemas/room_schema.py
@@ -4,47 +4,42 @@ from typing import Optional
 from datetime import datetime
 from enum import Enum
 
-from pydantic import BaseModel, constr, conint, condecimal
+from pydantic import BaseModel, constr, conint, condecimal-
 
 class RoomStatus(str, Enum):
 	available = "Available"
 	occupied = "Occupied"
 	maintenance = "Maintenance" 
 	
+# Base schema for shared fields
 class RoomBase(BassModel):
 	room_number: constr(min_length=1, max_length=10)
 	room_type_id: int 
-	floor_number: conint(gt=0)
+	floor_number: conint(ge=0)
 	price_per_night: condecimal(ge=0, max_digits=10, decimal_places=2)
 	room_status: RoomStatus = RoomStatus.available
 	room_description: Optional[str] = None
 	beds_count: conint(gt=0)
-	capacity: conint(get=1)
-	
+	capacity: conint(ge=1)
+
+# For creating a new room	
 class RoomCreate(RoomBase):
 	pass
 	
+# For updating room details
 class RoomUpdate(BaseModel):
 	room_number: Optional[constr(min_length=1, max_length=10)] = None
 	room_type_id: Optional[int] = None 
 	floor_number: Optional[conint(ge=0) = None
 	price_per_night: Optional[condecimal(ge=0, max_digits=10, decimal_places=2)] = None 
 	room_status: Optional[RoomStatus] = None 
-    room_description: Optionl[str] = None
+    room_description: Optional[str] = None
 	beds_count: optional[conint[gt=0] = None 
 	capacity: Optional[conint(ge=1)] = None 
     
-
+# For returing room info
 class RoomOut(BaseModel):
 	room_id: int
-	room_number: str
-	room_type_id: int 
-	floor_number: int
-	price_per_night: float
-	room_status: RoomStatus
-	room_description: Optional[str]
-	beds_count: int 
-	capacity: int
 	created_at: datetime
 	updated_at: datetime 
 	
