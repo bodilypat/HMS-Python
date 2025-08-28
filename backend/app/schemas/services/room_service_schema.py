de3e27d33a25d675fab51b35f9dd6f40df92b7c3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-28 09:10:41 -0700

    Add files via upload

--- backend/app/schemas/services/room_service_schema.py
+++ backend/app/schemas/services/room_service_schema.py
@@ -18,37 +18,53 @@ class RoomServiceBase(BaseModel):
     reservation_id: int 
     service_id: int 
     service_request_time: Optional[datetime] = Field (
-        default=None, description="Time when service was requested (optional)"
+        default=None, 
+        description="Time when service was requested (optional)"
     )
     service_status: RoomServiceStatus = Field (
-        default=RoomServiceStatus.requested, description="Current status of the service"
+        default=RoomServiceStatus.requested, 
+        description="Current status of the service request."
     )
-    notes: Optional[str] = Field(default=None, max_length = 1000)
+    notes: Optional[str] = Field(
+        default=None, 
+        max_length = 1000
+        description="Optional notes or special instructuons for the service."
+    )
     
 # CREATE SCHEMA
 class RoomServiceCreate(RoomServiceBase):
+    """
+        Schema used then creating a new rom service request.
+    """
     pass 
 
 # UPDATE SCHEMA    
 class RoomServiceUpdate(BaseModel):
-    service_request_time: Optional[datetime] = None 
-    service_status: Optional[RoomServiceStatus] = None 
-    notes: Optional[str] = Field(default=None, max_length=1000)
+    service_request_time: Optional[datetime] = Field (
+        default=None,
+        description="Updated request time, if changed." 
+    ) 
+    service_status: Optional[RoomServiceStatus] = Field (
+        default=None,
+        description="Updated service status."
+    )
+    notes: Optional[str] = Field(
+        default=None, 
+        max_length=1000,
+        description="Updated notes, if any."
+    )
+    
+# OUTPUT / RESPONSE SCHEMA
+class RoomServiceOut(BaseModel):
+    room_service_id: int 
+    reservation_id: int,
+    service_id: int
+    service_request_time: datetime
+    service_status: RoomServiceStatus
+    notes: Optional[str] = None 
+    created_at: datetime
+    updated_at: datetime
     
     class Config:
         orm_mode = Ture
 
-# OUTPUT SCHEMA    
-class RoomServiceOut(BaseModel):
-    room_service_id: int 
-    reservation_id: int 
-    service_id: int 
-    service_request_time: datetime 
-    service_status: RoomServiceStatus
-    notes: Optional[str] = None
-    created_at: datetime
-    updated_at: datetime
-    
-class Config:
-    orm_mode = True
-    
