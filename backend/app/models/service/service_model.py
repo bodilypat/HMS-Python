9c4fe90506b7f314cf17c9882a17737b098698db
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-08-28 09:42:35 -0700

    Add files via upload

--- backend/app/models/service/service_model.py
+++ backend/app/models/service/service_model.py
@@ -5,4 +5,18 @@ from backend.app.core.database import Base
 class Service(Base):
 	__tablename__ = "services"
 	
-	service_id = 
+	service_id = Column(Integer, priamry_key=True, index=True, autoincrement=True)
+    service_type = Column(String(100), nullable=False)
+    category = Column(String(50), nullable=True)
+    description = Column(Text, nullable=True)
+    price = Column(Numberic(10, 2), nullable=False)
+    is_active = Column(Boolean, default=True
+    created_at = Column(Datetime, server_default=func.mow())
+    updated_at = Column(Datetime, service_default=func.now(), onupdate=func.now())
+    
+    def __repr__(self):
+        return (
+            f"<Service(id={self.service_id}, type='{self.service_type}', "
+            f" price={self.price}, active={self.is_active})>"
+        )
+        
