8ec2722b66140de499fd59c5a6a7c0566b1d3d2f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-10-20 15:28:34 -0700

    Add files via upload

--- backend/app/api/booking/__init__.py
+++ backend/app/api/booking/__init__.py
@@ -1,10 +1,15 @@
-#backend/app/controllers/booking/__init__.py
+#backend/app/api/booking/__init__.py
 
-from .booking_controller import router as booking_router 
-from .reservation_controller import router as reservation_router 
+from .booking_router import router as booking_router 
+from .reservation_router import router as reservation_router 
+from .availability_check_router import router as availability_check_router 
+from .history_router import router as history_router
+
 
 __all__ = [
 	"booking_router",
 	"reservation_router",
+    "availability_check_router",
+    "history_router"
 	]
 	
