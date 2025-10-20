f517587258a02151b22d7f0300621df2870c4af4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-10-20 16:20:30 -0700

    Create __init__.py

---
+++ backend/app/services/amenities/__init__.py
@@ -0,0 +1,7 @@
+from .hotel_amenity_service import service as hotel_amenity_service 
+from .room_amenity_service import service as room_amenity_service 
+
+__all__ = [
+  "hotel_amenity_service",
+  "room_amenity_service",
+]
