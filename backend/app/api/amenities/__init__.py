a6db6cd887191e45d5b8c26f6082b7003ba9301b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-12-12 12:32:39 -0800

    Delete backend/app/api/amenities directory

--- backend/app/api/amenities/__init__.py
+++
@@ -1,9 +0,0 @@
-#backend/app/api/amenities/__init__.py
-
-from .hotel_amenity_router import router as hotel_amenity_router
-from .room_amenity_router import router as room_amenity_router 
-
-__all__ = [
-	"hotel_amenity_router",
-	"room_amenity_router",
-	]
