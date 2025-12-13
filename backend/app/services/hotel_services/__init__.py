0edc2019b07a6f672cf81028e1cddb28f977c41d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-12-12 21:18:03 -0800

    Add files via upload

---
+++ backend/app/services/hotel_services/__init__.py
@@ -0,0 +1,10 @@
+#app/services/hotel_services/__init__.py
+
+from .additional_service import AdditionalService
+from .room_service_log import RoomServiceLogService 
+
+__all__ = [
+    "AdditionalService",
+    "RoomServiceLogService"
+]
+
